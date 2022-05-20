7f9a94fe190f26ed8f70ff94a9e88d972f61aa19
bgvlrd <bivelarde@up.edu.ph>
2022-05-20 11:29:37 +0800

    restyled messages framework

--- UPBank/settings.py
+++ UPBank/settings.py
@@ -85,6 +85,13 @@ DATABASES = {
     }
 }
 
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-secondary',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+ }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
