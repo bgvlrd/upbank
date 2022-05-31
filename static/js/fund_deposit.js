d7e5db9aae5e59c8ba3c221769b62ac342158be9
unknown <jstence@up.edu.ph>
2022-05-31 12:59:03 +0800

    Reverting because of bugs

--- static/js/fund_deposit.js
+++ static/js/fund_deposit.js
@@ -24,6 +24,7 @@ $("#fill-button").click(function(){
 		$(".review").removeClass("d-none");
 
 		var loan = $("#loan-number :selected").text();
+		console.log("LOAN:" + loan);
         var amt = $("#amt").val();
 
         $("#loan-number-review").text(loan.toString());
@@ -38,6 +39,7 @@ $("#back-button").click(function(){
 	$(".fill").removeClass("d-none");
 });
 
+
 $("#submit-final").click(function(e){
 	e.preventDefault();
 
@@ -46,33 +48,6 @@ $("#submit-final").click(function(e){
 		url: deposit_url,
 		data: {
 			amt: $("#amt").val()
-		},
-		success:function(data){
-			$(".review").addClass("d-none");
-			$(".success-submit").removeClass("d-none");
-			$("#date-success").text(data["date"]);
-			$("#time-success").text(data["time"]);
-			$("#holder-success").text(data["full_name"]);
-			$("#acctnumber-success").text(data["account_number"]);
-			$("#amt-success").text("PHP " + data["amt_deposited"]);
-			$("#balance-success").text("PHP " + data["balance"]);
-		},
-		error: function() {
-			swal("Something's wrong.", "There is an error with the server. Try again later.", "error");
-		}
-	});
-});
-
-$("#otc-submit-final").click(function(e){
-	e.preventDefault();
-
-	$.ajax({
-		type: 'POST',
-		url: payment_url,
-		data: {
-			amt: $("#amt").val()
-			date: date
-			loan: $("#loan-number :selected").text()
 		},
 		success:function(data){
 			$(".review").addClass("d-none");
